f498623d32409b06e43062924e3e748f2076fa65
Mejbaur Bahar Fagun <fagun115946@gmail.com>
2021-07-08 22:54:55 +0600

    Add files via upload

---
+++ cat_dog.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+PATH = 'cats_and_dogs'
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+# Get number of files in each directory. The train and validation directories
+# each have the subdirecories "dogs" and "cats".
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+total_test = len(os.listdir(test_dir))
+
+# Variables for pre-processing and training.
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+rescale=1/255
+
+train_image_generator = ImageDataGenerator(rescale=rescale)
+validation_image_generator = ImageDataGenerator(rescale=rescale)
+test_image_generator = ImageDataGenerator(rescale=rescale)
+
+train_data_gen = train_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    directory=train_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    directory=validation_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='binary')
+
+test_data_gen = test_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    classes=["."], # this is the trick bit
+    directory=test_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    shuffle=False,
+    class_mode="binary")
+    
+  def plotImages(images_arr, probabilities = False):
+    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
+    if probabilities is False:
+      for img, ax in zip( images_arr, axes):
+          ax.imshow(img)
+          ax.axis('off')
+    else:
+      for img, probability, ax in zip( images_arr, probabilities, axes):
+          ax.imshow(img)
+          ax.axis('off')
+          if probability > 0.5:
+              ax.set_title("%.2f" % (probability*100) + "% dog")
+          else:
+              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
+    plt.show()
+
+sample_training_images, _ = next(train_data_gen)
+plotImages(sample_training_images[:5])
